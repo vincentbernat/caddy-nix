1dfa83719a75e0dc97d0c99066e45dbae0cf9448
Vincent Bernat <vincent@bernat.ch>
2024-11-10 23:51:37 +0100

    Remove version post-check
    This is not really needed. The name of a fixed-output version is also
    important. If there is a mismatch, it is compiled again. This is not
    done for fetchurl and the like because the default name is "source".

--- caddy.nix
+++ caddy.nix
@@ -43,17 +43,6 @@ final: prev: rec {
             outputHash = hash;
             outputHashAlgo = "sha256";
           };
-          # As we rely on pkgs.caddy, which version may change on upgrades, we
-          # may silently reuse the previous version because the above derivation
-          # would be cached. The check below is a workaround to avoid that and
-          # ensure the version we want was really the one built. This is a
-          # common problem with Nix. See: https://github.com/NixOS/nix/issues/7999.
-          postCheck = (oldAttrs.postCheck or "") + ''
-            [ "$(cat version)" = "v${version}" ] || {
-              echo "Mismatched version $(cat version) in source, while v${version} requested"
-              exit 1
-            }
-          '';
         });
     };
   });
